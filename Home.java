6743344868d86ea6dde1f4d1c059c4e1ad397bfd
jeyavan <selvinive@gmail.com>
2023-06-10 00:29:37 -1200

    second project

---
+++ Home.java
@@ -0,0 +1,37 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope(value="prototype")      //we can call object many times using with prototype scope
+
+public class Home {
+public Home() {
+	System.out.println("Home Home");
+	}
+	//POJO class
+	private String owner;
+	private int doorNo;
+	@Autowired
+	@Qualifier("III")
+	private InternetConnection modem;     // modem object
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public int getDoorNo() {
+		return doorNo;
+	}
+	public void setDoorNo(int doorNo) {
+		this.doorNo = doorNo;
+	}
+	
+	public void connect() {
+		
+		modem.switchOn();
+		System.out.println("Connecting to Internet");
+	}
+	
+}
